02c99431b46e86347b540c90d0e06ba1d8d67d21
IzumiSy <beetle-noise@gmx.com>
2025-10-22 17:03:42 +0900

    Fix env access

--- apps/frontend/deploy.mjs
+++ apps/frontend/deploy.mjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env zx
 
-await $`tailorctl workspace staticwebsite deploy --name main-app --directory ./dist -w ${WORKSPACE_ID}`.pipe(
+await $`tailorctl workspace staticwebsite deploy --name main-app --directory ./dist -w ${process.env.WORKSPACE_ID}`.pipe(
   process.stdout
 );
