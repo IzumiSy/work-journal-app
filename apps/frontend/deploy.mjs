8046e315b9d40b6918b0f73b1503af2f37823fa2
IzumiSy <beetle-noise@gmx.com>
2025-10-22 17:01:36 +0900

    Set workspace ID

--- apps/frontend/deploy.mjs
+++ apps/frontend/deploy.mjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env zx
 
-await $`tailorctl workspace staticwebsite deploy --name main-app --directory ./dist`.pipe(
+await $`tailorctl workspace staticwebsite deploy --name main-app --directory ./dist -w ${WORKSPACE_ID}`.pipe(
   process.stdout
 );
